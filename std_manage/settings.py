b043b2092f822c356d73a692c1e7c57433252f8d
Himanshut1806 <himanshut1806@gmail.com>
2025-02-14 11:18:49 +0530

    firstcommit

--- std_manage/settings.py
+++ std_manage/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fw$n_pt6!^&_k)y-gpw(dlnoq)slvk7z228h3utz4qrehff^$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
